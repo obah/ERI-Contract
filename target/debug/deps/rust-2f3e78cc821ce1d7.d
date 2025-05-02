1fff6aac8cb1b8d5522b6d09328d4ac335eed78f
Dean <o.michaeldean@gmail.com>
2025-05-02 03:40:29 +0100

    removed unnecessary files

--- target/debug/deps/rust-2f3e78cc821ce1d7.d
+++
@@ -1,5 +0,0 @@
-/home/dean/Solidity_Projects/ERI_Contract/rust/target/debug/deps/librust-2f3e78cc821ce1d7.rmeta: src/main.rs
-
-/home/dean/Solidity_Projects/ERI_Contract/rust/target/debug/deps/rust-2f3e78cc821ce1d7.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/rust-476f7e035c12d06c.d
+++
@@ -1,5 +0,0 @@
-/home/dean/Solidity_Projects/ERI_Contract/rust/target/debug/deps/librust-476f7e035c12d06c.rmeta: src/main.rs
-
-/home/dean/Solidity_Projects/ERI_Contract/rust/target/debug/deps/rust-476f7e035c12d06c.d: src/main.rs
-
-src/main.rs:
