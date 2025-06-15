e17c11f6c47aaee88832c644c9fe655f100712f0
Dean <o.michaeldean@gmail.com>
2025-06-15 17:42:00 +0100

    Did all the final touches

--- script/deploy.js
+++ script/deploy.js
@@ -42,14 +42,14 @@ main().catch((error) => {
 });
 
 // 📚 OwnershipLib deployed at: 0x3fB3BCdd95271Fc8b7ebFF48d4d7541b982355ef
-// https://sepolia.basescan.org/address/0x1684d002f846febCd6A21DB15C71642532a4edAf#code
+// https://sepolia.basescan.org/address/0x3fB3BCdd95271Fc8b7ebFF48d4d7541b982355ef#code
 
 
 // 📦 Ownership deployed at: 0x49e8207450dd0204Bb6a89A9edf7CE151cE58BBc
-// https://sepolia.basescan.org/address/0x184ecbE6d79710215c9C8Ce0757875714D78d96c#code
+// https://sepolia.basescan.org/address/0x49e8207450dd0204Bb6a89A9edf7CE151cE58BBc#code
 
 // 🧾 Authenticity deployed at: 0x98BC72046616b528D4Bc5bbcC7d99f82237A8B55
-// https://sepolia.basescan.org/address/0x8C3F22cAf6fA0879c04889D6fb5e38747c0f75FC#code
+// https://sepolia.basescan.org/address/0x98BC72046616b528D4Bc5bbcC7d99f82237A8B55#code
 
 // to verify a contract, you need the contract address and also the constructor parameters
 // npx hardhat verify --network base 0xf36f55D6Df2f9d5C7829ed5751d7E88FD3E82c2E 0xF2E7E2f51D7C9eEa9B0313C2eCa12f8e43bd1855 0x527caBd4bb83F94f1Fc1888D0691EF95e86795A1
